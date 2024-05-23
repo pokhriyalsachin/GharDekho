bc6998cfb2f19283a8fb9288c65c0682c41d1016
Sachin Pokhriyal <99538862+pokhriyalsachin@users.noreply.github.com>
2024-05-23 18:21:23 +0530

    Update user.controller.js
    ek aur mistake

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/errorhandler.js";
 
